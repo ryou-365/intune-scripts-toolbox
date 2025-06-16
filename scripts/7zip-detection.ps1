0dbec328808cbb0cb6a8cd42a7356ebfefdfa5c2
ryou_365 <111210650+ryou-365@users.noreply.github.com>
2025-06-16 09:53:44 +0900

    Add Test-Path check to 7zip detection

--- scripts/7zip-detection.ps1
+++ scripts/7zip-detection.ps1
@@ -1,14 +1,22 @@
-$FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\7-Zip\7z.exe").FileVersion
-#The below line trims the spaces before and after the version name
-$FileVersion = $FileVersion.Trim();
+$filePath = "C:\Program Files\7-Zip\7z.exe"
+
+if (-not (Test-Path $filePath)) {
+    Write-Output "7-Zip executable not found at $filePath"
+    exit 1
+}
+
+$FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($filePath).FileVersion
+# The below line trims the spaces before and after the version name
+$FileVersion = $FileVersion.Trim()
+
 if ("24.09" -eq $FileVersion)
 {
-#Write the version to STDOUT by default
-$FileVersion
-exit 0
+    # Write the version to STDOUT by default
+    $FileVersion
+    exit 0
 }
 else
 {
-#Exit with non-zero failure code
-exit 1
+    # Exit with non-zero failure code
+    exit 1
 }
