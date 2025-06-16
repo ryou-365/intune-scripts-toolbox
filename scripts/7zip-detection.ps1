cd9b9601d2e7b2cd985a47fb367d2e912f13c90d
ryou_365 <111210650+ryou-365@users.noreply.github.com>
2025-06-16 09:54:10 +0900

    Add version parameter to 7zip detection script

--- scripts/7zip-detection.ps1
+++ scripts/7zip-detection.ps1
@@ -1,7 +1,10 @@
+param(
+    [string]$ExpectedVersion = "24.09"
+)
 $FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\7-Zip\7z.exe").FileVersion
 #The below line trims the spaces before and after the version name
 $FileVersion = $FileVersion.Trim();
-if ("24.09" -eq $FileVersion)
+if ($ExpectedVersion -eq $FileVersion)
 {
 #Write the version to STDOUT by default
 $FileVersion
